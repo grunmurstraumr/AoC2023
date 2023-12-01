9684dc65b062b4dacfe5b6da0827681dc51e7586
Erik <erik.grundstrom@hotmail.com>
2023-12-01 23:12:16 +0100

    Almost working partTwo

--- app/src/main/java/se/eg/aoc/Day_1.java
+++ app/src/main/java/se/eg/aoc/Day_1.java
@@ -4,14 +4,26 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.lang.Character.isDigit;
 
 public class Day_1 {
+
+    private static final Map<String, String> DIGITS = Map.of(
+            "one", "1",
+            "two", "2",
+            "three", "3",
+            "four", "4",
+            "five", "5",
+            "six", "6",
+            "seven", "7",
+            "eight", "8",
+            "nine", "9"
+    );
+
     public static String partOne(String input)
     {
         List<String> digits =  Arrays.stream(input.split("")).filter(s -> isDigit(s.charAt(0))).toList();
@@ -31,4 +43,52 @@ public class Day_1 {
         }
         return 0;
     }
+
+
+    public static String partTwo(String input)
+    {
+        while (true)
+        {
+            Optional<String> firstNumber = findFirstMatchingKey(input);
+            if (firstNumber.isEmpty())
+                break;
+            input = input.replaceAll(firstNumber.get(), DIGITS.get(firstNumber.get()));
+        }
+        List<String> digits = Arrays.stream(input.split("")).filter(s -> isDigit(s.charAt(0))).toList();
+        String returnValue = digits.getFirst() + digits.getLast();
+        return returnValue;
+    }
+
+    private static Optional<String> findFirstMatchingKey(String input)
+    {
+        Map<Integer, String> matchIndex = new HashMap<>();
+        for (String key : DIGITS.keySet())
+        {
+            int index = input.indexOf(key);
+            if (index >= 0) {
+                matchIndex.put(index, key);
+            }
+        }
+        if (matchIndex.isEmpty()) {
+            return Optional.empty();
+        }
+        Optional<Integer> lowestIndex = matchIndex.keySet().stream().sorted().findFirst();
+        return Optional.of(matchIndex.get(lowestIndex.get()));
+    }
+
+    public static int runPartTwo(String filename)
+    {
+        List<String> lines;
+        try {
+            Day_1.class.getClassLoader().getResource(filename).toURI();
+            lines = Files.readAllLines(Path.of(Day_1.class.getClassLoader().getResource(filename).toURI()));
+            return lines.stream().map(Day_1::partTwo).mapToInt(Integer::parseInt).sum();
+        }
+        catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
 }
--- app/src/test/java/se/eg/aoc/Day_1Test.java
+++ app/src/test/java/se/eg/aoc/Day_1Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,9 +27,29 @@ public class Day_1Test {
                 Arguments.of("treb7uchet", "77"));
     }
 
+    @ParameterizedTest
+    @MethodSource("partTwoTestData")
+    void testPartTwo(String input, String expected)
+    {
+        assertEquals(expected, Day_1.partTwo(input));
+    }
+
+    private static Stream<Arguments> partTwoTestData() {
+        return Stream.of(
+                Arguments.of("two1nine", "29"),
+                Arguments.of("eightwothree", "83"),
+                Arguments.of("abcone2threexyz", "13"),
+                Arguments.of("xtwone3four", "24"),
+                Arguments.of("4nineeightseven2", "42"),
+                Arguments.of("zoneight234", "14"),
+                Arguments.of("7pqrstsixteen", "76"),
+                Arguments.of("18sixlone", "11"),
+                Arguments.of("6twodndmhcgxlgbqbqndbbthnngblfgtzh5fouroneightrjp", "68"),
+                Arguments.of("oneight", "18"));
+    }
     @Test
     public void run()
     {
-        System.out.println(Day_1.runPartOne("input.txt"));
+        System.out.println(Day_1.runPartTwo("input.txt"));
     }
 }
