ba512891a05654ed7e6982962bcff76162513ff1
Erik <erik.grundstrom@hotmail.com>
2023-12-02 23:28:29 +0100

    Day one part two works

--- app/src/main/java/se/eg/aoc/Day_1.java
+++ app/src/main/java/se/eg/aoc/Day_1.java
@@ -12,83 +12,101 @@ import static java.lang.Character.isDigit;
 
 public class Day_1 {
 
-    private static final Map<String, String> DIGITS = Map.of(
-            "one", "1",
-            "two", "2",
-            "three", "3",
-            "four", "4",
-            "five", "5",
-            "six", "6",
-            "seven", "7",
-            "eight", "8",
-            "nine", "9"
-    );
+    private static final Map<String, String> DIGITS = new HashMap<>();
+    static {
+        for (Integer i = 1; i < 10; i++) {
+            DIGITS.put(i.toString(), i.toString());
+        }
+        DIGITS.put("one", "1");
+        DIGITS.put("two", "2");
+        DIGITS.put("three", "3");
+        DIGITS.put("four", "4");
+        DIGITS.put("five", "5");
+        DIGITS.put("six", "6");
+        DIGITS.put("seven", "7");
+        DIGITS.put("eight", "8");
+        DIGITS.put("nine", "9");
+    }
 
-    public static String partOne(String input)
-    {
-        List<String> digits =  Arrays.stream(input.split("")).filter(s -> isDigit(s.charAt(0))).toList();
+    public static String partOne(String input) {
+        List<String> digits = Arrays.stream(input.split("")).filter(s -> isDigit(s.charAt(0))).toList();
         return digits.getFirst() + digits.getLast();
     }
 
-    public static int runPartOne(String filename)
-    {
+    public static int runPartOne(String filename) {
         List<String> lines;
         try {
             Day_1.class.getClassLoader().getResource(filename).toURI();
             lines = Files.readAllLines(Path.of(Day_1.class.getClassLoader().getResource(filename).toURI()));
             return lines.stream().map(Day_1::partOne).mapToInt(Integer::parseInt).sum();
-        }
-        catch (IOException | URISyntaxException e) {
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
         return 0;
     }
 
 
-    public static String partTwo(String input)
-    {
-        while (true)
-        {
-            Optional<String> firstNumber = findFirstMatchingKey(input);
-            if (firstNumber.isEmpty())
+    public static String partTwo(String input) {
+        int ithMatch = 0;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            Optional<Map<String, Integer>> digit = findIthMatchingKey(input, ithMatch);
+            if (digit.isEmpty())
                 break;
-            input = input.replaceAll(firstNumber.get(), DIGITS.get(firstNumber.get()));
+            String s = digit.get().keySet().stream().findFirst().get();
+            int index = digit.get().values().stream().findFirst().get();
+            sb.append(DIGITS.get(s));
+//            input = input.substring(0, index ) + input.substring(index + s.length());
+            ithMatch++;
         }
-        List<String> digits = Arrays.stream(input.split("")).filter(s -> isDigit(s.charAt(0))).toList();
+        List<String> digits = List.of(sb.toString().split(""));
         String returnValue = digits.getFirst() + digits.getLast();
         return returnValue;
     }
 
-    private static Optional<String> findFirstMatchingKey(String input)
-    {
-        Map<Integer, String> matchIndex = new HashMap<>();
-        for (String key : DIGITS.keySet())
-        {
-            int index = input.indexOf(key);
-            if (index >= 0) {
-                matchIndex.put(index, key);
+    private static Optional<Map<String, Integer>> findIthMatchingKey(String input, int i) {
+        SortedMap<Integer, String> matchIndex = new TreeMap<>();
+        for (String key : DIGITS.keySet()) {
+            List<Integer> indexes = findAllOccurences(input, key);
+            if (!indexes.isEmpty()) {
+                indexes.forEach(index -> matchIndex.put(index, key));
             }
         }
         if (matchIndex.isEmpty()) {
             return Optional.empty();
         }
-        Optional<Integer> lowestIndex = matchIndex.keySet().stream().sorted().findFirst();
-        return Optional.of(matchIndex.get(lowestIndex.get()));
+
+        if (i >= matchIndex.size()) {
+            return Optional.empty();
+        }
+        var value = (Map.Entry<Integer, String>) matchIndex.entrySet().toArray()[i];
+        return Optional.of(Map.of(value.getValue(), value.getKey()));
     }
 
-    public static int runPartTwo(String filename)
-    {
+    static List<Integer> findAllOccurences(String input, String target) {
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < input.length() - target.length() + 1; i++){
+            if (target.equals(input.substring(i, i + target.length()))){
+                indexes.add(i);
+            }
+        }
+        return indexes;
+    }
+
+    public static int runPartTwo(String filename) {
         List<String> lines;
         try {
             Day_1.class.getClassLoader().getResource(filename).toURI();
             lines = Files.readAllLines(Path.of(Day_1.class.getClassLoader().getResource(filename).toURI()));
-            return lines.stream().map(Day_1::partTwo).mapToInt(Integer::parseInt).sum();
-        }
-        catch (IOException | URISyntaxException e) {
+            return sumPartTwo(lines);
+        } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
         return 0;
     }
 
-
+    static int sumPartTwo(List<String> input)
+    {
+        return input.stream().map(Day_1::partTwo).mapToInt(Integer::parseInt).sum();
+    }
 }
--- app/src/test/java/se/eg/aoc/Day_1Test.java
+++ app/src/test/java/se/eg/aoc/Day_1Test.java
@@ -42,11 +42,55 @@ public class Day_1Test {
                 Arguments.of("xtwone3four", "24"),
                 Arguments.of("4nineeightseven2", "42"),
                 Arguments.of("zoneight234", "14"),
-                Arguments.of("7pqrstsixteen", "76"),
+                Arguments.of("7pqrstsixteen", "76"));
+    }
+
+    @Test
+    void partTwoTestSum() {
+        Stream<Arguments> args = partTwoTestData();
+        var l = args.map(Arguments::get).map(o -> (String) o[0]).toList();
+        assertEquals(281, Day_1.sumPartTwo(l));
+    }
+
+    @ParameterizedTest
+    @MethodSource("partTwoAdditionalTestData")
+    void testPartTwoAdditionalData(String input, String expected)
+    {
+        assertEquals(expected, Day_1.partTwo(input));
+    }
+
+    @Test
+    void testAllOccurences()
+    {
+        var indexes = Day_1.findAllOccurences("nineonenineonenine", "nine");
+        assertEquals(3, indexes.size());
+    }
+
+    private static Stream<Arguments> partTwoAdditionalTestData()
+    {
+        return Stream.of(
                 Arguments.of("18sixlone", "11"),
                 Arguments.of("6twodndmhcgxlgbqbqndbbthnngblfgtzh5fouroneightrjp", "68"),
-                Arguments.of("oneight", "18"));
+                Arguments.of("oneight", "18"),
+                Arguments.of("85sevenfivesixslkjbninefournine", "89"),
+                Arguments.of("seven98", "78"),
+                Arguments.of("444lxgckhmhxd19ngrhftc", "49"),
+                Arguments.of("six4two9", "69"),
+                Arguments.of("fourthreerqcnchsfourthreefourgghcmcjvfv4", "44"),
+                Arguments.of("vd6seven946h", "66"),
+                Arguments.of("6vzpjhtwonemc", "61"),
+                Arguments.of("33sevenmggst", "37"),
+                Arguments.of("nine67", "97"),
+                Arguments.of("two5five895ccsxdphn4", "24"),
+                Arguments.of("768", "78"),
+                Arguments.of("33sevenmggst", "37"),
+                Arguments.of("33sevenmggst", "37"),
+                Arguments.of("33sevenmggst", "37"),
+                Arguments.of("33sevenmggst", "37"),
+                Arguments.of("33sevenmggst", "37"),
+                Arguments.of("1twooneone1four", "14"));
     }
+
     @Test
     public void run()
     {
